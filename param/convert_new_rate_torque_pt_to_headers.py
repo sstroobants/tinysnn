870e12ac2918f2f7fc40d5a2745d0b429556e512
Stein Stroobants <s.stroobants@tudelft.nl>
2024-01-16 15:42:16 +0100

    new model trained on lower gains

--- param/convert_new_rate_torque_pt_to_headers.py
+++ param/convert_new_rate_torque_pt_to_headers.py
@@ -7,7 +7,7 @@ from convert_pt_utils import create_from_template, create_connection_from_templa
 if __name__ == "__main__": 
     # Load network
     rate_state_dict = torch.load(f"param/models/model_rate_decent_snowball.pt") # gyro combination
-    torque_state_dict = torch.load(f"param/models/model_torque_pleasant_morning.pt")
+    torque_state_dict = torch.load(f"param/models/model_torque_stilted_microwave.pt")
 
     # rate_hidden_size = rate_state_dict["enc.neuron.leak_i"].size()[0]
     rate_hidden_size = rate_state_dict["l1.neuron.leak_i"].size()[0]
